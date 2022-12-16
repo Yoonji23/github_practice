7b8a23cd913c74be9880a33cfa367b276af96491
djwjfk <grw07236@naver.com>
2022-12-16 19:48:52 +0900

    fix:dev연습

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
 
 function App() {
   return (
@@ -17,9 +19,17 @@ function App() {
         >
           Learn React
         </a>
+      <Div>
+        <span>Click!</span>
+      </Div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const Div = styled.div`
+border-radius: 50%;
+background-color: blue;
+`
